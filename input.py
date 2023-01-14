ea297c90c7bb002639d7d8b07aeb5829867c4397
Gokulr08 <gokul08r@gmail.com>
2023-01-15 00:11:20 +0530

    first commit

---
+++ input.py
@@ -0,0 +1,15 @@
+# get a input from user
+
+a=int(input("Enter value for a :")) # get integer
+print(a)
+
+name=input("Enter your name:") # Get string from user 
+print(name)
+
+
+# Check input type 
+
+print("Value for a is:",a," ",type(a))
+print("your name is",name,type(name))
+
+
