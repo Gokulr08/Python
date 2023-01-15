e9cefcf0e47f7f6e0ff73e039480a53243ba6a83
Gokulr08 <gokul08r@gmail.com>
2023-01-15 20:48:43 +0530

    first commit

--- input.py
+++ input.py
@@ -1,15 +1,56 @@
 # get a input from user
+num = int(input("Enter value for num :")) # get integer
+print(num)
 
-a=int(input("Enter value for a :")) # get integer
-print(a)
-
-name=input("Enter your name:") # Get string from user 
+name = input("Enter your name:") # Get string from user 
 print(name)
 
 
 # Check input type 
+print("Value for a is:",num," ",type(num))
+print("your name is",name," ",type(name))
 
-print("Value for a is:",a," ",type(a))
-print("your name is",name,type(name))
+# input from console
+num1 = input()
+num2 = input()
 
+# print
+print(num1 + num2)
+5
+# taking multiple input at a time from user
+# using split()
+x, y = input("Enter two values: ").split()
 
+print("Value for x is", x)
+print("Value for y is", y)
+
+# using format()
+x, y = input("Enter two number:").split()
+print("first number is {} and second number is {}".format(x, y))
+
+# using list()
+numbers = list(map(int, input("Enter numbers:").split()))
+print("List of boys:", x)
+
+# taking multiple values 
+# using list comprehension
+
+'''
+newlist = [ expression(element) for element 
+in oldlist if condion ]
+'''
+
+# example 1
+num = [2,4,5,6,7]
+sqr = [n**2 for n in num]
+print(sqr) 
+
+# example 2
+n_letters = [letter for letter in "gokul"]
+print(n_letters)  
+
+# example
+ipl = ['chennai', 'mumbai', 'bangalore', 'pune']
+newlist = [x for x in ipl if 'a' in x] # using with for loop 
+
+print(newlist)
