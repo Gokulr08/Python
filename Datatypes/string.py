9dd6793f0dc6deb4e8b7084795a9a6c649074351
Gokulr08 <gokul08r@gmail.com>
2023-01-20 21:01:10 +0530

    example

---
+++ Datatypes/string.py
@@ -0,0 +1,28 @@
+# string
+a = "Gokul"
+print(a)
+
+# get a specific character
+b = 'Gokul'
+print(a[2])
+
+# reverse a string
+print(a[::-1])
+
+# looping through string
+for i in "Gokul":
+    print(i)
+
+# string length
+c = "Gokul"
+print(len(c))
+
+# check string
+d = "Chennai super kings"
+if 'super' in d: #if-else method
+    print("yes, super is in d")
+
+print('Chennai' in d)
+
+# check if not
+print("gokul" not in d)
